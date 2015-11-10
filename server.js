edb522d42ff58a1a74082aee144ea0ff38a370d0
HXE8629 <Hayden_erickson@homedepot.com>
2015-11-10 10:09:52 -0500

    read from heroku config var for prod env or local DB for dev

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 var express = require( 'express' );
+var mysql = require( 'mysql' );
 var app = express();
 
+var connection = mysql.createConnection( 
+	process.env.CLEARDB_DATABASE_URL ||
+	{
+		host : 'localhost',
+		port : 3306,
+		user : 'root',
+		database : 'test'
+	}
+);
+	
+	
 app.get( '/', (req, res) => 
 {
-	res.send( "hello dude!" );
+	connection.connect();
+	
+	connection.query( 'SELECT * FROM tbl_users;', 
+		function( err, rows, fields )
+		{
+			if( err ) 
+			{
+				res.send( 'error: ' + err );
+				return;
+			}
+			
+			res.send( 'rows: ' + JSON.stringify( rows[0] ) );
+		}
+	);
+	
+	connection.end();
+	
 });
 
 app.listen( 6060, ()=> console.log( 'app is listening on 6060' ) );
+
+
